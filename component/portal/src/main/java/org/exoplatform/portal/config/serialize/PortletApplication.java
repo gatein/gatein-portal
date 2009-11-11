2f6719203dec425f4572f1ece2e68435638055f3
Julien Viet <julien@julienviet.com>
2009-11-11 00:33:06 +0000

    forgot to commit one class

---
+++ component/portal/src/main/java/org/exoplatform/portal/config/serialize/PortletApplication.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2003-2007 eXo Platform SAS.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.portal.config.serialize;
+
+import org.exoplatform.portal.config.model.Application;
+import org.exoplatform.portal.config.model.ApplicationType;
+import org.exoplatform.portal.pom.data.ApplicationData;
+import org.exoplatform.portal.pom.spi.portlet.Preferences;
+
+/**
+ * We keep that subclass here to please JiBX crap.
+ *
+ * @author <a href="mailto:julien.viet@exoplatform.com">Julien Viet</a>
+ * @version $Revision$
+ */
+public class PortletApplication extends Application<Preferences>
+{
+
+   public PortletApplication(ApplicationData<Preferences> preferencesApplicationData)
+   {
+      super(preferencesApplicationData);
+   }
+
+   public PortletApplication(ApplicationType<Preferences> preferencesApplicationType, String storageId)
+   {
+      super(preferencesApplicationType, storageId);
+   }
+
+   public PortletApplication(ApplicationType<Preferences> preferencesApplicationType)
+   {
+      super(preferencesApplicationType);
+   }
+}
