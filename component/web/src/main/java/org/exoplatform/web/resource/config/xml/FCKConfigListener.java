2b1392f7c948c7a06321c63613e732945e9f5e5c
tanpd <pdtanit@gmail.com>
2009-12-31 13:16:36 +0000

    FCK is useless in Gatein. Remove it

---
+++ component/web/src/main/java/org/exoplatform/web/resource/config/xml/FCKConfigListener.java
@@ -0,0 +1,20 @@
+package org.exoplatform.web.resource.config.xml;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class FCKConfigListener implements ServletContextListener
+{
+   public static String FCK_CONTEXT_PATH;
+
+   public void contextDestroyed(ServletContextEvent event)
+   {
+      
+   }
+
+   public void contextInitialized(ServletContextEvent event)
+   {
+      FCK_CONTEXT_PATH = event.getServletContext().getContextPath();
+   }
+   
+}
