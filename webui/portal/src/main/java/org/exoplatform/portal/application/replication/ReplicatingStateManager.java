d2933b8d38924d5a69800d069107dcc1058022fb
mwringe <mwringe@gmail.com>
2010-02-24 16:43:44 +0000

    Use the portal owner as part of the portal session key to differentiate between multiple portals when accessing a portal application state (GTNPORTAL-650)

--- webui/portal/src/main/java/org/exoplatform/portal/application/replication/ReplicatingStateManager.java
+++ webui/portal/src/main/java/org/exoplatform/portal/application/replication/ReplicatingStateManager.java
@@ -154,7 +154,8 @@ public class ReplicatingStateManager extends StateManager
       else
       {
          PortalRequestContext portalRC = (PortalRequestContext)webuiRC;
-         return "portal";
+         String portalOwner = portalRC.getPortalOwner();
+         return "portal_" + portalOwner;
       }
    }
 
