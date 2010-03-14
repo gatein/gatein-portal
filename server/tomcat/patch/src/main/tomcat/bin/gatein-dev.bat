385d203b5cf24500bf6fa98322726099117c365e
Thomas Heute <theute@gmail.com>
2010-03-14 19:19:59 +0000

    Windows doesn't like the quotes

--- server/tomcat/patch/src/main/tomcat/bin/gatein-dev.bat
+++ server/tomcat/patch/src/main/tomcat/bin/gatein-dev.bat
@@ -25,11 +25,13 @@ for %%i in ( !%~f0! ) do set BIN_DIR=%%~dpi
 cd %BIN_DIR%
 
 rem Sets some variables
-set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
-set SECURITY_OPTS="-Djava.security.auth.login.config=..\conf\jaas.conf"
-set EXO_OPTS="-Dexo.product.developing=false -Dexo.conf.dir.name=gatein\conf"
-set EXO_CONFIG_OPTS="-Dorg.exoplatform.container.configuration.debug"
-set JAVA_OPTS=-Xms128m -Xmx512m -XX:MaxPermSize=256m %LOG_OPTS% %SECURITY_OPTS% %EXO_OPTS% %EXO_CONFIG_OPTS%
+set LOG_OPTS=-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog
+set SECURITY_OPTS=-Djava.security.auth.login.config=..\conf\jaas.conf
+set EXO_OPTS=-Dexo.product.developing=true -Dexo.conf.dir.name=gatein\conf
+set EXO_CONFIG_OPTS=-Dorg.exoplatform.container.configuration.debug
+set REMOTE_DEBUG=-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n
+
+set JAVA_OPTS=-Xms128m -Xmx512m -XX:MaxPermSize=256m %LOG_OPTS% %SECURITY_OPTS% %EXO_OPTS% %EXO_CONFIG_OPTS% %REMOTE_DEBUG%
 set JPDA_TRANSPORT=dt_socket
 set JPDA_ADDRESS=8000
 
--- server/tomcat/patch/src/main/tomcat/bin/gatein.bat
+++ server/tomcat/patch/src/main/tomcat/bin/gatein.bat
@@ -25,10 +25,10 @@ for %%i in ( !%~f0! ) do set BIN_DIR=%%~dpi
 cd %BIN_DIR%
 
 rem Sets some variables
-set LOG_OPTS="-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog"
-set SECURITY_OPTS="-Djava.security.auth.login.config=..\conf\jaas.conf"
-set EXO_OPTS="-Dexo.product.developing=false -Dexo.conf.dir.name=gatein\conf"
-set JAVA_OPTS=-Xms128m -Xmx512m -XX:MaxPermSize=256m %LOG_OPTS% %SECURITY_OPTS% %EXO_OPTS%%
+set LOG_OPTS=-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog
+set SECURITY_OPTS=-Djava.security.auth.login.config=..\conf\jaas.conf
+set EXO_OPTS=-Dexo.product.developing=false -Dexo.conf.dir.name=gatein\conf
+set JAVA_OPTS=-Xms128m -Xmx512m -XX:MaxPermSize=256m %LOG_OPTS% %SECURITY_OPTS% %EXO_OPTS%
 
 rem Launches the server
 call catalina.bat %*
