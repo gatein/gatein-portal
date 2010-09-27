8a2525a757f7c335f349b8d9dfd6f2dc0b5cd62f
vuthelinh <vuthelinh@mycompany.com>
2010-09-27 06:50:49 +0000

    add ContextMenu

--- testsuite/selenium-snifftests/src/main/java/org/exoplatform/portal/selenium/SeleniumTestCaseGenerator.java
+++ testsuite/selenium-snifftests/src/main/java/org/exoplatform/portal/selenium/SeleniumTestCaseGenerator.java
@@ -194,7 +194,7 @@ public class SeleniumTestCaseGenerator {
 				sb.append("(selenium.isTextPresent(\"");
 				sb.append(param2);
 				sb.append("\"));\n");
-			} else if (param1.equals("click") || param1.equals("mouseDown") || param1.equals("doubleClick") || param1.equals("mouseDownRight")|| param1.equals("mouseUp")
+			} else if (param1.equals("click") ||param1.equals("contextMenu") || param1.equals("mouseDown") || param1.equals("doubleClick") || param1.equals("mouseDownRight")|| param1.equals("mouseUp")
 			      || param1.equals("open") || param1.equals("selectFrame") || param1.equals("selectWindow")) {
 				sb.append("selenium.");
 				sb.append(param1);
