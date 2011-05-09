3d08d3f8c709c355d1d9b4bf0e275c26077772d5
Julien Viet <julien@julienviet.com>
2011-05-09 14:09:37 +0000

    GTNPORTAL-1889 : fix configuration (it was broken due to me) + missing portlet.xml file for Jetty

--- component/pc/src/main/java/org/exoplatform/portal/pc/ExoPortletApplicationDeployer.java
+++ component/pc/src/main/java/org/exoplatform/portal/pc/ExoPortletApplicationDeployer.java
@@ -53,7 +53,7 @@ public class ExoPortletApplicationDeployer extends PortletApplicationDeployer
       {
          md.setResourceBundleFactoryName(ExoResourceBundleFactory.class.getName());
 
-         String globalPortletLocation = PropertyManager.getProperty("gatein.portlet.conf");
+         String globalPortletLocation = PropertyManager.getProperty("gatein.portlet.config");
          if (globalPortletLocation != null)
          {
             try
