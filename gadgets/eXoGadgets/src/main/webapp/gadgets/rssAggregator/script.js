9f7e0ca3221fe75193f275e6d33de1d2fcaf1ee4
haint <haithanh0809@gmail.com>
2011-09-30 08:04:01 +0000

    GTNPORTAL-2127 RSS Gadget Display time in correct format

--- gadgets/eXoGadgets/src/main/webapp/gadgets/rssAggregator/script.js
+++ gadgets/eXoGadgets/src/main/webapp/gadgets/rssAggregator/script.js
@@ -44,43 +44,7 @@ RssAggregator.prototype.timeToPrettyString = function(B) {
         return "an indeterminate amount of time ago"
     }
     time = (new Date().getTime()*1000 - B) / 1000;
-    if (time < 60) {
-        return "less than a minute ago"
-    } else {
-        if (time < 120) {
-            return "about a minute ago"
-        } else {
-            if (time < 3600) {
-                var A = Math.round(time / 60);
-                return "about " + A + " minutes ago"
-            } else {
-                if (time < 7200) {
-                    return "about an hour ago"
-                } else {
-                    if (time < 86400) {
-                        var A = Math.round(time / 3600);
-                        return "about " + A + " hours ago"
-                    } else {
-                        if (time < 172800) {
-                            return "about a day ago"
-                        } else {
-                            if (time < 2592000) {
-                                var A = Math.round(time / 86400);
-                                return "about " + A + " days ago"
-                            } else {
-                                if (time < 5184000) {
-                                    return "about a month ago"
-                                } else {
-                                    var A = Math.round(time / 2592000);
-                                    return "about " + A + " months ago"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    return (new Date(B).toLocaleString());
 }
 
 RssAggregator.prototype.renderFeed = function(feedObj) {
