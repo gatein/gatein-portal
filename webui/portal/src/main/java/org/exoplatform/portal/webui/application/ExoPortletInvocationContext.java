955e10efa4a139c0d46c7e45fdf7014f89e3c779
bdaw <boleslaw.dawidowicz@gmail.com>
2011-10-11 12:10:05 +0000

    GTNPORTAL-2164 Fix URL encoding issue that causes trouble with latest PortletBridge

--- webui/portal/src/main/java/org/exoplatform/portal/webui/application/ExoPortletInvocationContext.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/application/ExoPortletInvocationContext.java
@@ -22,6 +22,7 @@ package org.exoplatform.portal.webui.application;
 import org.exoplatform.Constants;
 import org.exoplatform.commons.utils.I18N;
 import org.exoplatform.portal.application.PortalRequestContext;
+import org.exoplatform.web.url.MimeType;
 import org.exoplatform.web.url.PortalURL;
 import org.exoplatform.webui.core.UIComponent;
 import org.exoplatform.webui.url.ComponentURL;
@@ -285,6 +286,10 @@ class ExoPortletInvocationContext extends AbstractPortletInvocationContext
 
       // Confirm messsage
       url.setConfirm(props.get("gtn:confirm"));
+
+      //
+      MimeType mimeType = Boolean.TRUE.equals(format.getWantEscapeXML()) ? MimeType.XHTML : MimeType.PLAIN;
+      url.setMimeType(mimeType);
 
       //
       return url.toString();
