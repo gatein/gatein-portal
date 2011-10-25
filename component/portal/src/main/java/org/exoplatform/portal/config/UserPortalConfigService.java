a291ca2188849529a40a3a4999721f36d3ffe3d0
mto <hoang281283@gmail.com>
2011-10-25 06:59:20 +0000

    GTNPORTAL-2197: Allow to tune cache easily

--- component/portal/src/main/java/org/exoplatform/portal/config/UserPortalConfigService.java
+++ component/portal/src/main/java/org/exoplatform/portal/config/UserPortalConfigService.java
@@ -44,7 +44,6 @@ import org.exoplatform.services.log.Log;
 import org.exoplatform.services.organization.Group;
 import org.exoplatform.services.organization.OrganizationService;
 import org.picocontainer.Startable;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
@@ -672,5 +671,15 @@ public class UserPortalConfigService implements Startable
    public String getDefaultPortal()
    {
       return newPortalConfigListener_.getDefaultPortal();
+   }
+
+   public Set<String> getPortalTemplates()
+   {
+      return newPortalConfigListener_.getTemplateConfigs(PortalConfig.PORTAL_TYPE);
+   }
+
+   public PortalConfig getPortalConfigFromTemplate(String templateName)
+   {
+      return newPortalConfigListener_.getPortalConfigFromTemplate(PortalConfig.PORTAL_TYPE, templateName);
    }
 }
