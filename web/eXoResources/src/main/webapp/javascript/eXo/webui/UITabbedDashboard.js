a10eca373b519f054484f62dd6b97c4e04263a7f
Minh Hoang TO <hoang281283@gmail.com>
2012-02-16 17:39:51 +0700

    Refactor UITabbedDashboard.js using JQuery

--- web/eXoResources/src/main/webapp/javascript/eXo/webui/UITabbedDashboard.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/webui/UITabbedDashboard.js
@@ -18,144 +18,120 @@
  */
 
 eXo.webui.UITabbedDashboard = {
-	
-	init : function(){eXo.webui.UITabbedDashboard.isInRequest = false;},
-	
-	renameTabLabel : function(e){
-		if(!e){
-			e = window.event;
-		}
-		var keyNum = e.keyCode;
-		
-		//If user presses on ENTER button, then rename the tab label
-		if(keyNum == 13){
-			var inputElement = eXo.core.Browser.getEventSource(e);
-			var newTabLabel = inputElement.value;
-			if(!newTabLabel || newTabLabel.length < 1){
-				return;
-			}
-			var DOMUtil = eXo.core.DOMUtil;
-			var portletFrag = DOMUtil.findAncestorByClass(inputElement, "PORTLET-FRAGMENT");
-			var compId = portletFrag.parentNode.id;
-			var nodeName = inputElement.id;
-			
-			//Send request to server to change node name
-			var href = eXo.env.server.portalBaseURL + "?portal:componentId=" + compId;
-			href += "&portal:type=action";
-			href += "&portal:isSecure=false";
-			href += "&uicomponent=UITabPaneDashboard";
-			href += "&op=RenameTabLabel";
-			href += "&objectId=" + nodeName;
-			href += "&newTabLabel=" + encodeURIComponent(newTabLabel);
-			window.location = href;
-		}
-		//If user presses on the ESCAPE key, then reset the span element on the tab
-		else if(keyNum == 27){
-			var inputElement = eXo.core.Browser.getEventSource(e);
-			if(eXo.webui.UITabbedDashboard.backupElement) {
- 				inputElement.parentNode.replaceChild(eXo.webui.UITabbedDashboard.backupElement, inputElement);
- 				eXo.webui.UITabbedDashboard.backupElement = null;
-			}
-		}
-	},
 
-	showEditLabelInput : function(selectedElement, nodeName, currentContent){
-		eXo.webui.UITabbedDashboard.backupElement = selectedElement;
-		var prNode = selectedElement.parentNode;
-		
-		var inputElement = document.createElement("input");
-		inputElement.type = "text";
-		inputElement.id = nodeName;
-		inputElement.name = currentContent; // To store old value
-		inputElement.value = currentContent;
-		inputElement.style.border = "1px solid #b7b7b7";
-		inputElement.style.width = (selectedElement.offsetWidth - 2 ) + "px";
-		inputElement.onkeypress = eXo.webui.UITabbedDashboard.renameTabLabel;
-		inputElement.setAttribute('maxLength', 50);
-		inputElement.onblur = function() {
-			prNode.replaceChild(eXo.webui.UITabbedDashboard.backupElement, inputElement);
-		};
-		
-		prNode.replaceChild(inputElement, selectedElement);
-		inputElement.focus();
-		
-		var DOMUtil = eXo.core.DOMUtil;
-		var uiTab = DOMUtil.findAncestorByClass(inputElement, "UITab");
-		DOMUtil.addClass(uiTab, "EditTab");	
-	},
-	
-	createDashboardPage : function(e){
-		if(!e){
-			e = window.event;
-		}	
-		if(eXo.webui.UITabbedDashboard.isInRequest) return;
-		var keyNum = e.keyCode;
-		
-		//If user presses on ENTER button
-		if(keyNum == 13){
-			var inputElement = eXo.core.Browser.getEventSource(e);
-			var newTabLabel = inputElement.value;
-			if(!newTabLabel || newTabLabel.length < 1) return;
-			
-			//Send request to server to change node name
-			var href = eXo.env.server.portalBaseURL + "?portal:componentId=" + inputElement.id;
-			href += "&portal:type=action";
-			href += "&portal:isSecure=false";
-			href += "&uicomponent=UITabPaneDashboard";
-			href += "&op=AddDashboard";
-			href += "&objectId=" + encodeURIComponent(newTabLabel);
-			eXo.webui.UITabbedDashboard.isInRequest = true;
-			window.location = href;
-		}
-		//If user presses on ESCAPE button
-		else if(keyNum == 27){
-			var inputElement = eXo.core.Browser.getEventSource(e);
-			var editingTabElement = eXo.core.DOMUtil.findAncestorByClass(inputElement, "UITab");
-			
-			//Remove the editing tab
-			editingTabElement.parentNode.removeChild(editingTabElement);
-		}
-	},
-	
-	cancelTabDashboard : function(e){
-		if(!e){
-			e = window.event;
-		}
-		var inputElement = eXo.core.Browser.getEventSource(e);
-		var editingTabElement = eXo.core.DOMUtil.findAncestorByClass(inputElement, "UITab");
-		
-		//Remove the editing tab
-		editingTabElement.parentNode.removeChild(editingTabElement);
-	},
-	
-	createTabDashboard : function(addTabElement){
-		var DOMUtil = eXo.core.DOMUtil;
-		var tabContainer = addTabElement.parentNode;
-		var tabElements = DOMUtil.findChildrenByClass(tabContainer, "div", "UITab");
-		var portletFrag = DOMUtil.findAncestorByClass(tabContainer, "PORTLET-FRAGMENT");
-		var selectedTabElement = DOMUtil.findFirstDescendantByClass(tabContainer, "div", "SelectedTab");
-		
-		var newTabElement = selectedTabElement.cloneNode(true);
-		tabContainer.insertBefore(newTabElement, addTabElement);
-		
-		var inputElement = document.createElement("input");
-		inputElement.type = "text";
-		inputElement.value = "Tab_" + tabElements.length;
-		inputElement.style.border = "1px solid #b7b7b7";
-		inputElement.style.width = "80px";
-		inputElement.onkeypress = eXo.webui.UITabbedDashboard.createDashboardPage;
-		inputElement.onblur = eXo.webui.UITabbedDashboard.cancelTabDashboard;
-		inputElement.setAttribute('maxLength', 50);
-		inputElement.id = portletFrag.parentNode.id; //Store the id of the portlet here
-		
-		var spanElement = DOMUtil.findDescendantsByTagName(newTabElement, "span")[0];
-		spanElement.parentNode.replaceChild(inputElement, spanElement);
-		
-		DOMUtil.findNextElementByTagName(inputElement, "a").href = "#";
-		inputElement.focus();	
-		
-		var DOMUtil = eXo.core.DOMUtil;
-		var uiTab = DOMUtil.findAncestorByClass(inputElement, "UITab");
-		DOMUtil.addClass(uiTab, "EditTab");	
-	}
+  /**
+   * A flag indicating that there is ongoing request to create new dashboard page. That helps
+   * us to avoid concurrent dashboard creating requests coming from one user.
+   *
+   */
+  inRequest : false,
+
+  renameTabLabel : function(input)
+  {
+    var newLabel = input.val();
+    if (newLabel && newLabel.length > 0)
+    {
+      var portletID = input.closest(".PORTLET-FRAGMENT").parent().attr("id");
+
+      var href = eXo.env.server.portalBaseURL + "?portal:componentId=" + portletID;
+      href += "&portal:type=action";
+      href += "&portal:isSecure=false";
+      href += "&uicomponent=UITabPaneDashboard";
+      href += "&op=RenameTabLabel";
+      href += "&objectId=" + input.attr("id");
+      href += "&newTabLabel=" + encodeURIComponent(newLabel);
+      window.location = href;
+    }
+  },
+
+  showEditLabelInput : function(target, nodeName, currentLabel)
+  {
+    var jqObj = xj(target);
+
+    var input = xj("<input>").attr({type : "text", id : nodeName, name : currentLabel, value : currentLabel, maxLength : 50});
+    input.css("border", "1px solid #b7b7b7").css("width", (target.offsetWidth - 2) + "px");
+
+    jqObj = jqObj.replaceWith(input);
+    input.blur(function()
+    {
+      xj(this).replaceWith(jqObj);
+    });
+
+    input.keypress(function(e)
+    {
+      var keyNum = e.keyCode ? e.keyCode : e.which;
+      if (keyNum == 13)
+      {
+        eXo.webui.UITabbedDashboard.renameTabLabel(xj(this));
+      }
+      else if (keyNum == 27)
+      {
+        xj(this).replaceWith(jqObj);
+      }
+    });
+
+    input.closest(".UITab").addClass("EditTab");
+    input.focus();
+  },
+
+  createTab : function(input)
+  {
+    if (this.inRequest)
+    {
+      return;
+    }
+    else
+    {
+      var label = input.val();
+      if (label && label.length > 0)
+      {
+        var href = eXo.env.server.portalBaseURL + "?portal:componentId=" + input.attr("id");
+        href += "&portal:type=action";
+        href += "&portal:isSecure=false";
+        href += "&uicomponent=UITabPaneDashboard";
+        href += "&op=AddDashboard";
+        href += "&objectId=" + encodeURIComponent(label);
+        this.InRequest = true;
+        window.location = href;
+      }
+    }
+  },
+
+  showAddTabInput : function(addButton)
+  {
+    var jqAddButton = xj(addButton);
+    var tabs = jqAddButton.parent().children("div.UITab");
+
+    var newTab = jqAddButton.prevAll("div.SelectedTab").eq(0).clone();
+    newTab.insertBefore(jqAddButton);
+
+    var portletID = jqAddButton.closest("div.PORTLET-FRAGMENT").parent().attr("id");
+
+    var input = xj("<input>").attr({type : "text", id : portletID , maxlength : 50, value : "Tab_" + tabs.length});
+    input.css({"border" : "1px solid #b7b7b7", "width" : "80px"});
+
+    newTab.find("span").eq(0).replaceWith(input);
+    input.next("a").attr("href", "#");
+
+    input.blur(function()
+    {
+      xj(this).closest(".UITab").remove();
+    });
+
+    input.keypress(function(e)
+    {
+      var keyNum = e.keyCode ? e.keyCode : e.which;
+      if (keyNum == 13)
+      {
+        eXo.webui.UITabbedDashboard.createTab(xj(this));
+      }
+      else if (keyNum == 27)
+      {
+        xj(this).closest(".UITab").remove();
+      }
+    });
+
+    input.closest(".UITab").addClass("EditTab");
+    input.focus();
+  }
 }
