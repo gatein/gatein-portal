358d251b30714c79b769103ebc994a63be8e80e6
haint <haithanh0809@gmail.com>
2012-05-10 11:03:51 +0700

    GTNPORTAL-875: Check user session user deleted by another

--- webui/portal/src/main/java/org/exoplatform/portal/application/UserProfileLifecycle.java
+++ webui/portal/src/main/java/org/exoplatform/portal/application/UserProfileLifecycle.java
@@ -20,44 +20,56 @@
 package org.exoplatform.portal.application;
 
 import org.exoplatform.container.ExoContainer;
+import org.exoplatform.portal.mop.SiteType;
 import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.UserProfile;
 import org.exoplatform.web.application.Application;
 import org.exoplatform.web.application.ApplicationLifecycle;
 import org.exoplatform.web.application.RequestFailure;
+import org.exoplatform.web.login.LogoutControl;
+import org.exoplatform.web.url.navigation.NavigationResource;
+import org.exoplatform.web.url.navigation.NodeURL;
 import org.exoplatform.webui.application.WebuiRequestContext;
 
 public class UserProfileLifecycle implements ApplicationLifecycle<WebuiRequestContext>
 {
    public static final String USER_PROFILE_ATTRIBUTE_NAME = "PortalUserProfile";
-	
+
    private final static ThreadLocal<UserProfile> currentUserProfile = new ThreadLocal<UserProfile>();
 
    @SuppressWarnings("unused")
    public void onInit(Application app)
    {
-	   //do nothing for now
+      // do nothing for now
    }
 
    @SuppressWarnings("unused")
    public void onStartRequest(final Application app, final WebuiRequestContext context) throws Exception
    {
-	   String user = context.getRemoteUser();
-	   UserProfile userProfile = null;
-	   if (user != null)
-	   {
-		   ExoContainer exoContainer = app.getApplicationServiceContainer();
-		   if (exoContainer != null)
-		   {
-			   OrganizationService organizationService = (OrganizationService) exoContainer
-			   .getComponentInstanceOfType(OrganizationService.class);
-			   userProfile = organizationService.getUserProfileHandler().findUserProfileByName(user);
-		   }
+      String user = context.getRemoteUser();
+      UserProfile userProfile = null;
+      if (user != null)
+      {
+         ExoContainer exoContainer = app.getApplicationServiceContainer();
+         if (exoContainer != null)
+         {
+            OrganizationService organizationService =
+               (OrganizationService)exoContainer.getComponentInstanceOfType(OrganizationService.class);
+            userProfile = organizationService.getUserProfileHandler().findUserProfileByName(user);
+         }
 
-	   }
+         if (userProfile == null)
+         {
+            LogoutControl.wantLogout();
+            PortalRequestContext prContext = (PortalRequestContext)context;
+            NodeURL createURL = prContext.createURL(NodeURL.TYPE);
+            createURL.setResource(new NavigationResource(SiteType.PORTAL, prContext.getPortalOwner(), null));
+            prContext.sendRedirect(createURL.toString());
+         }
+      }
 
-	   currentUserProfile.set(userProfile);
-	   context.setAttribute(this.USER_PROFILE_ATTRIBUTE_NAME, userProfile);
+      currentUserProfile.set(userProfile);
+      context.setAttribute(this.USER_PROFILE_ATTRIBUTE_NAME, userProfile);
    }
 
    @SuppressWarnings("unused")
@@ -69,13 +81,13 @@ public class UserProfileLifecycle implements ApplicationLifecycle<WebuiRequestCo
    @SuppressWarnings("unused")
    public void onEndRequest(Application app, WebuiRequestContext context) throws Exception
    {
-	   currentUserProfile.remove();
+      currentUserProfile.remove();
    }
 
    @SuppressWarnings("unused")
    public void onDestroy(Application app)
    {
-	   // do nothing for now
+      // do nothing for now
    }
 
 }
