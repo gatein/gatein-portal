69df0826be015027b1e6603b2dba1c641e151b6e
Minh Hoang TO <hoang281283@gmail.com>
2012-07-27 12:02:19 +0700

    GTNPORTAL-2479: JSF Portlet page doesn't re render after Ajax ActionRequest

--- web/eXoResources/src/main/webapp/javascript/eXo/core/Util.js
+++ web/eXoResources/src/main/webapp/javascript/eXo/core/Util.js
@@ -168,13 +168,6 @@ Array.prototype.pushAll = function (array) {
 	}
 } ;
 
-Array.prototype.each = function (iterator, context) {
-	iterator = iterator.bind(context);
-  	for (var i = 0; i < this.length; i++) {
-		iterator(this[i]) ;
-	}
-};
-
 /*************************************************************************/
 function  HashMap() { 
 	 this.properties = new Object() ;
