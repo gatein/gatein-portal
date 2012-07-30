1789ff275b122befcb786c13b32d9bbd66c3be56
phuong_vu <phuong.vu@exoplatform.com>
2012-07-30 14:36:10 +0700

    Make gatein-jquery example portlet work with requirejs

--- examples/portlets/jquery/src/main/webapp/javascript/version.js
+++ examples/portlets/jquery/src/main/webapp/javascript/version.js
@@ -1,15 +1,15 @@
-$(".version").remove();
-
-$("#portletJQuery").click(function()
-{
-  var target = $(this);
-  $('#result').append("<p>The JQuery's version: " + $().jquery + "</p>");
-  $('#result').children('p').fadeOut(3200);
+$(function() {
+	$("#portletJQuery").click(function() {
+		$('#result').append("<p>The JQuery's version: " + $().jquery + "</p>");
+		$('#result').children('p').fadeOut(3200);
+	});
 });
 
-$("#gateinJQuery").click(function()
+require(["SHARED/jquery"], function($) 
 {
-  var target = $(this);
-  gj('#result').append("<p>The JQuery's version: " + gj().jquery + "</p>");
-  gj('#result').children('p').fadeOut(3200);
+	$("#gateinJQuery").click(function() 
+	{
+		$('#result').append("<p>The JQuery's version: " + $().jquery + "</p>");
+		$('#result').children('p').fadeOut(3200);
+	});
 });
