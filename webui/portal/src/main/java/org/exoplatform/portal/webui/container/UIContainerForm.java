6ca5c3c5ea38f13673d22253a71b37adccd0a04b
phuong_vu <phuong.vu@exoplatform.com>
2012-10-11 16:03:50 +0700

    GTNPORTAL-2626 Missing translation in eXo Gatein while setting permission in portlet instances

--- webui/portal/src/main/java/org/exoplatform/portal/webui/container/UIContainerForm.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/container/UIContainerForm.java
@@ -85,7 +85,7 @@ public class UIContainerForm extends UIFormTabPane
       setSelectedTab(infoInputSet.getId());
 
       UIListPermissionSelector uiListPermissionSelector = createUIComponent(UIListPermissionSelector.class, null, null);
-      uiListPermissionSelector.configure("UIAccessPermission", "accessPermissions");
+      uiListPermissionSelector.configure("UIListPermissionSelector", "accessPermissions");
       uiListPermissionSelector.addValidator(EmptyIteratorValidator.class);
       UIFormInputSet uiPermissionSet = createUIComponent(UIFormInputSet.class, "UIContainerPermission", null);
       uiPermissionSet.addChild(uiListPermissionSelector);
