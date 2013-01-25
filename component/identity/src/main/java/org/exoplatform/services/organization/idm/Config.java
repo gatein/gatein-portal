d09d6b6f13d4df27c475957bee07d4533355934d
mposolda <mposolda@gmail.com>
2013-01-25 21:58:35 +0100

    GTNPORTAL-2806 Fix LDAP integration. Introduce options 'ignoreMappedMembershipTypeGroupList' and 'skipPaginationInMembershipQuery'
    GTNPORTAL-2806 Introduce option 'skipPaginationInMembershipQuery' to decide if we count paginated memberships or perform full query and filter memberships later

--- component/identity/src/main/java/org/exoplatform/services/organization/idm/Config.java
+++ component/identity/src/main/java/org/exoplatform/services/organization/idm/Config.java
@@ -21,8 +21,10 @@ package org.exoplatform.services.organization.idm;
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -50,7 +52,7 @@ public class Config {
 
     private String associationMembershipType;
 
-    private boolean ignoreMappedMembershipType = true;
+    private List<String> ignoreMappedMembershipTypeGroupList = new ArrayList<String>();
 
     private boolean useJTA = false;
 
@@ -59,6 +61,8 @@ public class Config {
     private boolean sortMemberships = true;
 
     private boolean countPaginatedUsers = true;
+
+    private boolean skipPaginationInMembershipQuery = false;
 
     public Config() {
     }
@@ -133,6 +137,32 @@ public class Config {
         }
 
         return null;
+    }
+
+    public boolean isIgnoreMappedMembershipTypeForGroup(String groupId) {
+        if ("/".equals(groupId)) {
+            return false;
+        }
+
+        String parentId = groupId.substring(0, groupId.lastIndexOf("/"));
+
+        for (String id : ignoreMappedMembershipTypeGroupList) {
+            // Check if any mapping that contains '/*' match this id
+            if (id.endsWith("/*")) {
+                id = id.substring(0, id.length() - 2);
+                // Check exact equality case
+            } else if (id.equals(groupId)) {
+                return true;
+            } else {
+                continue;
+            }
+
+            if (parentId.startsWith(id)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public String getPLIDMGroupName(String gtnGroupName) {
@@ -231,12 +261,12 @@ public class Config {
         this.associationMembershipType = associationMembershipType;
     }
 
-    public boolean isIgnoreMappedMembershipType() {
-        return ignoreMappedMembershipType;
+    public List<String> getIgnoreMappedMembershipTypeGroupList() {
+        return ignoreMappedMembershipTypeGroupList;
     }
 
-    public void setIgnoreMappedMembershipType(boolean ignoreMappedMembershipType) {
-        this.ignoreMappedMembershipType = ignoreMappedMembershipType;
+    public void setIgnoreMappedMembershipTypeGroupList(List<String> ignoreMappedMembershipTypeGroupList) {
+        this.ignoreMappedMembershipTypeGroupList = ignoreMappedMembershipTypeGroupList;
     }
 
     public boolean isUseJTA() {
@@ -277,5 +307,13 @@ public class Config {
 
     public void setCountPaginatedUsers(boolean countPaginatedUsers) {
         this.countPaginatedUsers = countPaginatedUsers;
+    }
+
+    public boolean isSkipPaginationInMembershipQuery() {
+        return skipPaginationInMembershipQuery;
+    }
+
+    public void setSkipPaginationInMembershipQuery(boolean skipPaginationInMembershipQuery) {
+        this.skipPaginationInMembershipQuery = skipPaginationInMembershipQuery;
     }
 }
