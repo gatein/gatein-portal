550f03c40fceedf2da333756bf3f10dc529cbfd4
Julien Viet <julien@julienviet.com>
2013-05-23 18:27:31 +0200

    Place holder for sitemap and register

---
+++ portal/ui/src/main/java/org/gatein/portal/ui/register/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * The portal web application.
+ */
+@Application
+@Bindings()
+@Portlet
+package org.gatein.portal.ui.register;
+
+import juzu.Application;
+import juzu.plugin.binding.Binding;
+import juzu.plugin.binding.Bindings;
+import juzu.plugin.portlet.Portlet;
+import org.gatein.portal.mop.description.DescriptionService;
+import org.gatein.portal.mop.navigation.NavigationService;
