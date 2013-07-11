26497c31f722de8f8f59a79f38b6987955b9d77e
nttuyen <tuyennt@exoplatform.com>
2013-07-11 17:02:56 +0700

    add comment as exo template

--- portal/web/src/main/java/org/gatein/portal/login/Controller.java
+++ portal/web/src/main/java/org/gatein/portal/login/Controller.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2012 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package org.gatein.portal.login;
 
 import java.io.IOException;
@@ -26,142 +45,139 @@ import org.gatein.wci.authentication.AuthenticationException;
 import org.gatein.wci.security.Credentials;
 import org.gatein.security.oauth.spi.OAuthProviderTypeRegistry;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tuyennt
- * Date: 7/3/13
- * Time: 3:55 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Controller {
-    /** . */
-    private static final int AUTHENTICATED = 1;
+  /**
+   * .
+   */
+  private static final int AUTHENTICATED = 1;
 
-    /** . */
-    private static final int FAILED = 2;
+  /**
+   * .
+   */
+  private static final int FAILED = 2;
 
-    @Inject
-    private Flash flash;
+  @Inject
+  private Flash flash;
 
-    @Inject
-    @Path("login.gtmpl")
-    private Template login;
+  @Inject
+  @Path("login.gtmpl")
+  private Template login;
 
-    @View
-    @Route(value = "/dologout", priority = 1)
-    public Response doLogout() {
-        try {
-            ServletContainer container = ServletContainerFactory.getServletContainer();
-            container.logout(Context.getCurrentRequest(), Context.getCurrentResponse());
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        HttpServletRequest req = Context.getCurrentRequest();
-        String initURL = req.getParameter("initURL");
-        if(initURL == null || initURL.isEmpty()) {
-            initURL = req.getContextPath();
-        }
+  @View
+  @Route(value = "/dologout", priority = 1)
+  public Response doLogout() {
+    try {
+      ServletContainer container = ServletContainerFactory.getServletContainer();
+      container.logout(Context.getCurrentRequest(), Context.getCurrentResponse());
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    HttpServletRequest req = Context.getCurrentRequest();
+    String initURL = req.getParameter("initURL");
+    if (initURL == null || initURL.isEmpty()) {
+      initURL = req.getContextPath();
+    }
+    return Response.redirect(initURL);
+  }
+
+  @View
+  @Route(value = "/dologin", priority = 1)
+  public Response doLogin() {
+    try {
+      HttpServletRequest req = Context.getCurrentRequest();
+      int status = req.getRemoteUser() != null ? AUTHENTICATED : FAILED;
+
+      String initURL = req.getParameter("initURL");
+      if (initURL == null || initURL.isEmpty()) {
+        initURL = req.getContextPath();
+      }
+
+      if (status == AUTHENTICATED) {
         return Response.redirect(initURL);
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
 
-    @View
-    @Route(value = "/dologin", priority = 1)
-    public Response doLogin() {
-        try {
-            HttpServletRequest req = Context.getCurrentRequest();
-            int status = req.getRemoteUser() != null ? AUTHENTICATED : FAILED;
+    String loginURL = Context.getCurrentRequest().getContextPath() + "/login";
+    return Response.redirect(loginURL);
+  }
 
-            String initURL = req.getParameter("initURL");
-            if(initURL == null || initURL.isEmpty()) {
-                initURL = req.getContextPath();
-            }
+  @Action
+  @Route(value = "/actionLogin", priority = 1)
+  public Response actionLogin(String username, String password) {
+    try {
+      Credentials credentials = new Credentials(username, password);
+      ServletContainer container = ServletContainerFactory.getServletContainer();
+      container.login(Context.getCurrentRequest(), Context.getCurrentResponse(), credentials);
+      return Controller_.doLogin();
+    } catch (ServletException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (AuthenticationException ex) {
+      ex.printStackTrace();
+    }
+    flash.setError("Username or password incorrect!");
+    return Controller_.login();
+  }
 
-            if (status == AUTHENTICATED) {
-                return Response.redirect(initURL);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+  @View
+  @Route(value = "/login", priority = 1)
+  public Response login(RequestContext context) {
+    ServletContext servletContext = null;
+    String contextPath = "";
+    try {
+      HttpServletRequest req = Context.getCurrentRequest();
+      servletContext = req.getServletContext();
+      contextPath = req.getContextPath();
 
-        String loginURL = Context.getCurrentRequest().getContextPath() + "/login";
-        return Response.redirect(loginURL);
+      int status = req.getRemoteUser() != null ? AUTHENTICATED : FAILED;
+
+      String initURL = req.getParameter("initURL");
+      if (initURL == null || initURL.isEmpty()) {
+        initURL = req.getContextPath();
+      }
+
+      String username = req.getParameter("username");
+      String password = req.getParameter("password");
+
+      if (status == AUTHENTICATED) {
+        return Response.redirect(initURL);
+
+      } else if (username != null && password != null) {
+        Credentials credentials = new Credentials(username, password);
+        ServletContainer container = ServletContainerFactory.getServletContainer();
+        container.login(Context.getCurrentRequest(), Context.getCurrentResponse(), credentials);
+        return Controller_.doLogin();
+      }
+    } catch (Exception ex) {
+      servletContext = null;
+      contextPath = context.getHttpContext().getContextPath();
     }
 
-    @Action
-    @Route(value = "/actionLogin", priority = 1)
-    public Response actionLogin(String username, String password) {
-        try {
-            Credentials credentials = new Credentials(username, password);
-            ServletContainer container = ServletContainerFactory.getServletContainer();
-            container.login(Context.getCurrentRequest(), Context.getCurrentResponse(), credentials);
-            return Controller_.doLogin();
-        } catch (ServletException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (AuthenticationException ex) {
-            ex.printStackTrace();
+    PortalContainer container = null;
+    if (servletContext == null) {
+      container = PortalContainer.getInstance();
+    } else {
+      container = PortalContainer.getCurrentInstance(servletContext);
+    }
+    OAuthProviderTypeRegistry registry = (OAuthProviderTypeRegistry) container.getComponentInstanceOfType(OAuthProviderTypeRegistry.class);
+
+    List<OauthProviderDescriptor> oauthProviders = null;
+    if (registry.isOAuthEnabled()) {
+      oauthProviders = new LinkedList<OauthProviderDescriptor>();
+      for (OAuthProviderType provider : registry.getEnabledOAuthProviders()) {
+        String type = "twitter";
+        if (provider.getKey().equals("GOOGLE")) {
+          type = "google-plus";
+        } else if (provider.getKey().equals("FACEBOOK")) {
+          type = "facebook";
         }
-        flash.setError("Username or password incorrect!");
-        return Controller_.login();
+        oauthProviders.add(new OauthProviderDescriptor(provider.getFriendlyName(), provider.getInitOAuthURL(contextPath), type));
+      }
     }
 
-    @View
-    @Route(value = "/login", priority = 1)
-    public Response login(RequestContext context) {
-        ServletContext servletContext = null;
-        String contextPath = "";
-        try {
-            HttpServletRequest req = Context.getCurrentRequest();
-            servletContext = req.getServletContext();
-            contextPath = req.getContextPath();
-
-            int status = req.getRemoteUser() != null ? AUTHENTICATED : FAILED;
-
-            String initURL = req.getParameter("initURL");
-            if(initURL == null || initURL.isEmpty()) {
-                initURL = req.getContextPath();
-            }
-
-            String username = req.getParameter("username");
-            String password = req.getParameter("password");
-
-            if (status == AUTHENTICATED) {
-                return Response.redirect(initURL);
-
-            } else if(username != null && password != null) {
-                Credentials credentials = new Credentials(username, password);
-                ServletContainer container = ServletContainerFactory.getServletContainer();
-                container.login(Context.getCurrentRequest(), Context.getCurrentResponse(), credentials);
-                return Controller_.doLogin();
-            }
-        } catch (Exception ex) {
-            servletContext = null;
-            contextPath = context.getHttpContext().getContextPath();
-        }
-
-        PortalContainer container = null;
-        if(servletContext == null) {
-            container = PortalContainer.getInstance();
-        } else {
-            container = PortalContainer.getCurrentInstance(servletContext);
-        }
-        OAuthProviderTypeRegistry registry = (OAuthProviderTypeRegistry) container.getComponentInstanceOfType(OAuthProviderTypeRegistry.class);
-
-        List<OauthProviderDescriptor> oauthProviders = null;
-        if(registry.isOAuthEnabled()) {
-            oauthProviders = new LinkedList<OauthProviderDescriptor>();
-            for(OAuthProviderType provider : registry.getEnabledOAuthProviders()) {
-                String type = "twitter";
-                if(provider.getKey().equals("GOOGLE")) {
-                    type = "google-plus";
-                } else if(provider.getKey().equals("FACEBOOK")) {
-                    type = "facebook";
-                }
-                oauthProviders.add(new OauthProviderDescriptor(provider.getFriendlyName(), provider.getInitOAuthURL(contextPath), type));
-            }
-        }
-
-        return login.with().set("oauthProviders", oauthProviders).ok().with(juzu.PropertyType.STYLESHEET, "login-stylesheet").with(juzu.PropertyType.STYLESHEET, "social-buttons");
-    }
+    return login.with().set("oauthProviders", oauthProviders).ok().with(juzu.PropertyType.STYLESHEET, "login-stylesheet").with(juzu.PropertyType.STYLESHEET, "social-buttons");
+  }
 }
--- portal/web/src/test/java/org/gatein/portal/PortalLoginTestCase.java
+++ portal/web/src/test/java/org/gatein/portal/PortalLoginTestCase.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2012 eXo Platform SAS.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
 package org.gatein.portal;
 
 import java.io.File;
@@ -35,13 +54,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tuyennt
- * Date: 7/4/13
- * Time: 10:35 AM
- * To change this template use File | Settings | File Templates.
- */
+
 @RunWith(Arquillian.class)
 public class PortalLoginTestCase extends AbstractPortalTestCase {
 
