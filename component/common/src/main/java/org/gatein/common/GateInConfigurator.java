2302902dc4f582ef8c3e772c0f8125816ab7779c
Julien Viet <julien@julienviet.com>
2013-08-02 10:42:43 +0200

    Don't override system properties when they already exist

--- component/common/src/main/java/org/gatein/common/GateInConfigurator.java
+++ component/common/src/main/java/org/gatein/common/GateInConfigurator.java
@@ -21,20 +21,44 @@ package org.gatein.common;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.Map;
 
 import org.exoplatform.commons.utils.PropertyManager;
-import org.exoplatform.container.PropertyConfigurator;
 import org.exoplatform.container.configuration.ConfigurationManager;
 import org.exoplatform.container.monitor.jvm.J2EEServerInfo;
+import org.exoplatform.container.util.ContainerUtil;
 import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.container.xml.PropertiesParam;
+import org.exoplatform.container.xml.Property;
+import org.exoplatform.container.xml.ValueParam;
 import org.gatein.common.logging.Logger;
 import org.gatein.common.logging.LoggerFactory;
+import org.picocontainer.Startable;
 
 /**
- * Extends the {@link org.exoplatform.container.PropertyConfigurator} to setup the virtual machine JAAS configuration
- * when it is not present (<code>java.security.auth.login.config</code> system property).
+ * <p>The property configurator configures a set of system properties via the {@link PropertyManager}
+ * static methods. It is possible to configure properties from the init params or from an external
+ * file. Existing system properties are preserved and not overidden.</p>
+ *
+ * <p>The constructor will inspect the {@link org.exoplatform.container.xml.InitParams} params argument
+ * to find a param named <code>properties</code> with an expected type of {@link PropertiesParam}. The
+ * properties contained in that argument will be sourced into the property manager. When such properties
+ * are loaded from an XML configuration file, the values are evaluated and property substitution occurs.</p>
+ *
+ * <p>When the property {@link PropertyManager#PROPERTIES_URL} is not null and points to a valid property
+ * file it will loaded and sourced. Property values will be evaluated and property substitution will
+ * occur. When the file name ends with the <code>.properties</code> properties are loaded using the
+ * {@link java.util.Properties#load(java.io.InputStream)} method. When the file name ends with th
+ * <code>.xml</code> properties are loaded using the {@link java.util.Properties#loadFromXML(java.io.InputStream)}
+ * method. Suffix checks are done ignoring the case.</p>
+ *
+ * <p>When properties are loaded from an URL, the order of the properties declarations in the file matters.</p>
+ *
+ * <p>In addition this configuration also configure the JAAS login configuration to setup the virtual machine
+ * JAAS configuration when it is not present (<code>java.security.auth.login.config</code> system property).</p>
  */
-public class GateInConfigurator extends PropertyConfigurator {
+public class GateInConfigurator implements Startable {
 
     /** . */
     private static final Logger log = LoggerFactory.getLogger(GateInConfigurator.class);
@@ -45,35 +69,95 @@ public class GateInConfigurator extends PropertyConfigurator {
     /** . */
     public static String DEFAULT_PATH = "/conf/jaas.conf";
 
-    public GateInConfigurator(ConfigurationManager confManager) {
-        this(null, confManager);
-    }
-
     public GateInConfigurator(InitParams params, ConfigurationManager confManager) {
-        super(params, confManager);
+
+        // Resolve the property file configuration path
+        String path = null;
+        if (params != null) {
+            PropertiesParam propertiesParam = params.getPropertiesParam("properties");
+            if (propertiesParam != null) {
+                log.debug("Going to initialize properties from init param");
+                for (Iterator<Property> i = propertiesParam.getPropertyIterator(); i.hasNext(); ) {
+                    Property property = i.next();
+                    String name = property.getName();
+                    String value = property.getValue();
+                    log.debug("Adding property from init param " + name + " = " + value);
+                    PropertyManager.setProperty(name, value);
+                }
+            }
+            ValueParam pathParam = params.getValueParam("properties.url");
+            if (pathParam != null) {
+                path = pathParam.getValue();
+                if (log.isDebugEnabled()) {
+                    log.debug("Using file path " + path + " found from configuration");
+                }
+            }
+        }
+        String systemPath = PropertyManager.getProperty(PropertyManager.PROPERTIES_URL);
+        if (systemPath != null) {
+            path = systemPath;
+            if (log.isDebugEnabled()) {
+                log.debug("Using file path " + path + " found from system properties");
+            }
+        }
+
+        //
+        if (path != null) {
+            log.debug("Found property file path " + path);
+            configureProperties(confManager, path);
+        }
 
         //
         if (PropertyManager.getProperty(JAAS_KEY) == null) {
+            configureJAAS();
+        }
+    }
 
-            // Determine the configuration path
-            String path = null;
-            File configFile = new File(new J2EEServerInfo().getServerHome() + DEFAULT_PATH);
-            if (configFile.exists()) {
-                // We use the explicit configuration
-                path = configFile.getAbsolutePath();
-            } else {
-                // Use the configuration URL from the jar
-                URL defConfig = Thread.currentThread().getContextClassLoader().getResource("conf/jaas.conf");
-                if (defConfig != null) {
-                    path = defConfig.toString();
+    private void configureProperties(ConfigurationManager confManager, String path) {
+        try {
+            URL url = confManager.getURL(path);
+            Map<String, String> props = ContainerUtil.loadProperties(url);
+            if (props != null) {
+                for (Map.Entry<String, String> entry : props.entrySet()) {
+                    String propertyName = entry.getKey();
+                    String propertyValue = entry.getValue();
+                    if (PropertyManager.getProperty(propertyName) == null) {
+                        log.debug("Set " + propertyName + "=" + propertyValue);
+                        PropertyManager.setProperty(propertyName, propertyValue);
+                    }
                 }
             }
+        } catch (Exception e) {
+            log.error("Cannot load property file " + path, e);
+        }
+    }
 
-            //
-            if (path != null) {
-                log.info("Setting JAAS configuration to " + path);
-                PropertyManager.setProperty(JAAS_KEY, path);
+    private void configureJAAS() {
+
+        // Determine the configuration jaas path
+        String jaasPath = null;
+        File configFile = new File(new J2EEServerInfo().getServerHome() + DEFAULT_PATH);
+        if (configFile.exists()) {
+            // We use the explicit configuration
+            jaasPath = configFile.getAbsolutePath();
+        } else {
+            // Use the configuration URL from the jar
+            URL defConfig = Thread.currentThread().getContextClassLoader().getResource("conf/jaas.conf");
+            if (defConfig != null) {
+                jaasPath = defConfig.toString();
             }
         }
+
+        //
+        if (jaasPath != null) {
+            log.info("Setting JAAS configuration to " + jaasPath);
+            PropertyManager.setProperty(JAAS_KEY, jaasPath);
+        }
+    }
+
+    public void start() {
+    }
+
+    public void stop() {
     }
 }
