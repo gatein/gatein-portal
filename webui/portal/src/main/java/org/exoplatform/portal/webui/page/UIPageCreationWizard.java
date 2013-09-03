2d0ac8db164254105d91822dfb67fcb61676fa8b
Peter Palaga <ppalaga@redhat.com>
2013-09-03 16:19:46 +0200

    GTNPORTAL-3252 Add new page wizard doesn't remember page layout

--- webui/portal/src/main/java/org/exoplatform/portal/webui/page/UIPageCreationWizard.java
+++ webui/portal/src/main/java/org/exoplatform/portal/webui/page/UIPageCreationWizard.java
@@ -334,7 +334,6 @@ public class UIPageCreationWizard extends UIPageWizard {
             uiPagePreview.setUIComponent(uiPage);
 
             uiWizard.updateWizardComponent();
-            uiPageTemplateOptions.setSelectedOption(null);
         }
     }
 
