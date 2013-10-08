c3c154fa93e1c71e93e88692538708a20d2ff8db
nttuyen <tuyennt@exoplatform.com>
2013-10-08 14:40:35 +0700

    add test updatelayout

--- component/portal/src/test/java/org/exoplatform/portal/mop/layout/TestLayout.java
+++ component/portal/src/test/java/org/exoplatform/portal/mop/layout/TestLayout.java
@@ -21,9 +21,20 @@ package org.exoplatform.portal.mop.layout;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.exoplatform.portal.config.model.ApplicationState;
+import org.exoplatform.portal.config.model.PersistentApplicationState;
 import org.exoplatform.portal.mop.AbstractMopServiceTest;
+import org.exoplatform.portal.pom.data.ApplicationData;
+import org.exoplatform.portal.pom.data.ComponentData;
+import org.exoplatform.portal.pom.data.ContainerAdapter;
+import org.exoplatform.portal.pom.data.ContainerData;
 import org.gatein.mop.core.util.Tools;
+import org.gatein.portal.mop.customization.CustomizationContext;
+import org.gatein.portal.mop.customization.CustomizationService;
 import org.gatein.portal.mop.hierarchy.NodeContext;
 import org.gatein.portal.mop.hierarchy.NodeData;
 import org.gatein.portal.mop.layout.Element;
@@ -74,6 +85,78 @@ public class TestLayout extends AbstractMopServiceTest {
         context.getSiteService().destroySite(site.key);
         assertEmptyLayout(layoutId);
 
+    }
+
+    public void testUpdateLayout() {
+        PageData page = createPage(createSite(SiteType.PORTAL, "test_layout_page"), "page", new PageState.Builder().build());
+        createElements(page, FOO_PORTLET, BAR_PORTLET);
+        String layoutId = page.layoutId;
+
+        NodeContext<Element, ElementState> pageContext = layoutService.loadLayout(Element.MODEL, layoutId, null);
+        assertEquals(2, pageContext.getNodeSize());
+        Element foo = pageContext.getNode(0);
+        Element bar = pageContext.getNode(1);
+        testInfo(foo, bar);
+
+        //Load layout tree
+        NodeContext<ComponentData, ElementState> pageStruct = (NodeContext<ComponentData, ElementState>)layoutService.loadLayout(ElementState.model(), layoutId, null);
+        assertEquals(2, pageStruct.getNodeSize());
+
+        Iterator<NodeContext<ComponentData, ElementState>> iterator = pageStruct.iterator();
+
+        //Window 1
+        NodeContext<ComponentData, ElementState> win1 = iterator.next();
+        ElementState.Window win1State = (ElementState.Window) win1.getState();
+        ApplicationData app1 = new ApplicationData(win1.getId(), win1.getName(), win1State.type, win1State.state, win1.getId(), win1State.properties.get(ElementState.Window.TITLE), win1State.properties.get(ElementState.Window.ICON), win1State.properties.get(ElementState.Window.DESCRIPTION), win1State.properties.get(ElementState.Window.SHOW_INFO_BAR), win1State.properties.get(ElementState.Window.SHOW_APPLICATION_STATE), win1State.properties.get(ElementState.Window.SHOW_APPLICATION_MODE), win1State.properties.get(ElementState.Window.THEME), win1State.properties.get(ElementState.Window.WIDTH), win1State.properties.get(ElementState.Window.HEIGHT), Collections.EMPTY_MAP, win1State.accessPermissions);
+
+        //Window 2
+        NodeContext<ComponentData, ElementState> win2 = iterator.next();
+        ElementState.Window win2State = (ElementState.Window) win2.getState();
+        ApplicationData app2 = new ApplicationData( win2.getId(), win2.getName(), win2State.type, win2State.state, win2.getId(), win2State.properties.get(ElementState.Window.TITLE), win2State.properties.get(ElementState.Window.ICON), win2State.properties.get(ElementState.Window.DESCRIPTION), false, false, false, win2State.properties.get(ElementState.Window.THEME), win2State.properties.get(ElementState.Window.WIDTH), win2State.properties.get(ElementState.Window.HEIGHT), Collections.EMPTY_MAP, win2State.accessPermissions);
+
+        //Rebuild children with barPortlet before fooPortlet
+        List<ComponentData> childrens = new LinkedList<ComponentData>();
+        childrens.add(app2);
+        childrens.add(app1);
+
+        //Rebuild tree
+        ElementState.Container rootState = (ElementState.Container) pageStruct.getState();
+        ContainerData rootData = new ContainerData( pageStruct.getId(), pageStruct.getName(), pageStruct.getId(), rootState.getProperties().get(ElementState.Container.NAME), rootState.getProperties().get(ElementState.Container.ICON), rootState.getProperties().get(ElementState.Container.TEMPLATE), rootState.getProperties().get(ElementState.Container.FACTORY_ID), rootState.getProperties().get(ElementState.Container.TITLE), rootState.getProperties().get(ElementState.Container.DESCRIPTION), rootState.getProperties().get(ElementState.Container.WIDTH), rootState.getProperties().get(ElementState.Container.HEIGHT), rootState.getAccessPermissions(), childrens);
+
+        layoutService.saveLayout(new ContainerAdapter(rootData), rootData, pageStruct, null);
+
+
+        pageContext = layoutService.loadLayout(Element.MODEL, layoutId, null);
+        assertEquals(2, pageContext.getNodeSize());
+        bar = pageContext.getNode(0);
+        foo = pageContext.getNode(1);
+        testInfo(foo, bar);
+    }
+
+
+    private void testInfo(Element foo, Element bar) {
+        if (foo == null || bar == null) {
+            fail("Foo and Bar component must no be null");
+        }
+        ElementState.Window fooWindow = (ElementState.Window) foo.getState();
+        assertEquals(Collections.singletonList("foo_access_permissions"), fooWindow.accessPermissions);
+        assertEquals("foo_title", fooWindow.properties.get(ElementState.Window.TITLE));
+        assertEquals("foo_description", fooWindow.properties.get(ElementState.Window.DESCRIPTION));
+        assertEquals("foo_theme", fooWindow.properties.get(ElementState.Window.THEME));
+        assertEquals("foo_height", fooWindow.properties.get(ElementState.Window.HEIGHT));
+        assertEquals("foo_width", fooWindow.properties.get(ElementState.Window.WIDTH));
+
+        assertTrue(foo.getState() instanceof ElementState.Window);
+        ApplicationState fooState = ((ElementState.Window) foo.getState()).state;
+        assertTrue(fooState instanceof PersistentApplicationState);
+
+        CustomizationService cusService = context.getCustomizationService();
+        PersistentApplicationState pState = (PersistentApplicationState)fooState;
+        CustomizationContext customContext = cusService.loadCustomization(pState.getStorageId());
+        assertNotNull(customContext);
+        assertNotNull(customContext.getContentId());
+
+        assertEquals("bar", ((ElementState.Window) bar.getState()).properties.get(ElementState.Window.TITLE));
     }
 
     public void testPage() {
