a7e6c3ed19079869aeb7c7a02d7ae01cbdf3dac6
haint <haithanh0809@gmail.com>
2013-12-12 15:49:33 +0700

    GTNPORTAL-3346: Message for disabled user when searching in Group Management

--- portlet/exoadmin/src/main/java/org/exoplatform/organization/webui/component/UIGroupMembershipForm.java
+++ portlet/exoadmin/src/main/java/org/exoplatform/organization/webui/component/UIGroupMembershipForm.java
@@ -199,13 +199,18 @@ public class UIGroupMembershipForm extends UIForm {
             for (String username : userNames) {
                 if (username == null || username.trim().length() == 0)
                     continue;
-                User user = service.getUserHandler().findUserByName(username);
+                User user = service.getUserHandler().findUserByName(username, false);
                 if (user == null) {
                     check = true;
                     if (listNotExist == null)
                         listNotExist = username;
                     else
                         listNotExist += ", " + username;
+                } else if (!user.isEnabled()){
+                    ApplicationMessage msg = new ApplicationMessage("UIGroupMembershipForm.msg.user-is-disabled", new String[] { user.getUserName()});
+                    msg.setArgsLocalized(false);
+                    uiApp.addMessage(msg);
+                    return;
                 }
             }
             if (check) {
