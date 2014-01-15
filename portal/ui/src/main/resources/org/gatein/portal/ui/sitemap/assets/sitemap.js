60c02f629d8d08f042d8e2b42b2c7f5f6e212617
haint <haithanh0809@gmail.com>
2014-01-15 09:31:39 +0700

    Require jquery for sitemap.js

--- portal/ui/src/main/resources/org/gatein/portal/ui/sitemap/assets/sitemap.js
+++ portal/ui/src/main/resources/org/gatein/portal/ui/sitemap/assets/sitemap.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-	(function(){
-		var sitemapContent = $('.sitemapUnit').find('.sitemapContent');
-		sitemapContent.children('.node:last').removeClass().addClass('lastNode node');
-		sitemapContent.find('.childrenContainer').each(function(){
-			$(this).children('.node:last').removeClass().addClass('lastNode node');
-		});
-		sitemapContent.find('.node').each(function(){
-			$(this).children('.expandItem').click(function(event) {
-				$(this).next('.childrenContainer').slideToggle('slow');
-				$(this).toggleClass('collapseItem');
-			})
-		});
-	})();
+require(['jquery'], function($) {
+  $(document).ready(function() {
+    (function(){
+      var sitemapContent = $('.sitemapUnit').find('.sitemapContent');
+      sitemapContent.children('.node:last').removeClass().addClass('lastNode node');
+      sitemapContent.find('.childrenContainer').each(function(){
+        $(this).children('.node:last').removeClass().addClass('lastNode node');
+      });
+      sitemapContent.find('.node').each(function(){
+        $(this).children('.expandItem').click(function(event) {
+          $(this).next('.childrenContainer').slideToggle('slow');
+          $(this).toggleClass('collapseItem');
+        })
+      });
+    })();
+  });
 });
