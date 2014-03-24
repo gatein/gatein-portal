dadb93ffbe0c364fcaa8e27e05fff9b8337fe547
Lucas Ponce <lponce@redhat.com>
2014-03-24 16:51:14 +0100

    GTNPORTAL-3429 Minor fixes for menu.js

--- mobile-integration/portlets/header/src/main/webapp/js/menu.js
+++ mobile-integration/portlets/header/src/main/webapp/js/menu.js
@@ -6,20 +6,19 @@
 
     $(".gtnResponsiveMenuCollapseButton").each(function(){
 
-            $(this).click(function(){
-                var dataTarget = $(this).children(".data.target").first();
-                var dataTargetClass = $(this).children(".data.target.class").first();
-                var dataAction = $(this).children(".data.action").first();
-                var dataSelfClass = $(this).children(".data.self.class").first();
+            $(this).click(function() {
+                var dataTarget = $(this).children(".data.target").length > 0 ? $(this).children(".data.target").first() : null;
+                var dataTargetClass = $(this).children(".data.target.class").length > 0 ? $(this).children(".data.target.class").first() : null;
+                var dataAction = $(this).children(".data.action").length > 0 ? $(this).children(".data.action").first() : null;
+                var dataSelfClass = $(this).children(".data.self.class").length > 0 ? $(this).children(".data.self.class").first() : null;
 
-                if (dataTarget && dataTargetClass && dataAction.val() == "toggleCSS")
-                {
-                    $(dataTarget.val()).toggleClass(dataTargetClass.val());
-                }
-
-                if (dataSelfClass && dataAction.val() == "toggleCSS")
-                {
-                    $(this).toggleClass(dataSelfClass.val());
+                if (dataAction) {
+                    if (dataTarget && dataTargetClass && dataAction.val() == "toggleCSS") {
+                        $(dataTarget.val()).toggleClass(dataTargetClass.val());
+                    }
+                    if (dataSelfClass && dataAction.val() == "toggleCSS") {
+                        $(this).toggleClass(dataSelfClass.val());
+                    }
                 }
             });
 
