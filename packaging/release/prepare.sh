2d9605c1605e0a1c62f4edb1ad9bd1ce53623599
Peter Palaga <ppalaga@redhat.com>
2014-04-16 14:03:53 +0200

    Update release scripts to match EAP rather than AS7

--- packaging/release/prepare.sh
+++ packaging/release/prepare.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Check if version param was passed
-if [[ ! ("$#" == 1)]]; then 
+if [[ ! ("$#" == 1)]]; then
     echo 'Please pass a version number as parameter'
     exit 1
 fi
 
 export VERSION=$1
 export NAME_PREFIX=GateIn-$VERSION
-export NAME_AS5=$NAME_PREFIX-jbossas5
-export NAME_AS6=$NAME_PREFIX-jbossas6
-export NAME_AS7=$NAME_PREFIX-jbossas7
+export NAME_EAP=$NAME_PREFIX-jboss-eap
 export NAME_TC6=$NAME_PREFIX-tomcat6
 export NAME_TC7=$NAME_PREFIX-tomcat7
 export NAME_JETTY=$NAME_PREFIX-jetty
@@ -21,11 +19,11 @@ echo 'Removing old stuff...'
 rm -rf upload
 rm -rf test
 
-# Copy all 
+# Copy all
 mkdir ./test
 mkdir ./test/server-bundles
 echo 'Copying all packaged servers into ./test/server-bundles/'
-cp -R ../jboss-as7/pkg/target/jboss ./test/server-bundles/$NAME_AS7
+cp -R ../jboss/pkg/target/jboss-eap ./test/server-bundles/$NAME_EAP
 cp -R ../tomcat/tomcat7/target/tomcat ./test/server-bundles/$NAME_TC7
 #cp -R ../jetty/pkg/target/jetty ./test/server-bundles/$NAME_JETTY
 
@@ -35,7 +33,7 @@ mkdir upload/server-bundles
 mkdir upload/server-bundles/$VERSION
 echo 'Creating server bundles in ./upload/server-bundles/'
 cd ./test/server-bundles
-zip -r -q ./../../upload/server-bundles/$VERSION/$NAME_AS7.zip $NAME_AS7 -x "*.DS_Store"
+zip -r -q ./../../upload/server-bundles/$VERSION/$NAME_EAP.zip $NAME_EAP -x "*.DS_Store"
 zip -r -q ./../../upload/server-bundles/$VERSION/$NAME_TC7.zip $NAME_TC7 -x "*.DS_Store"
 #zip -r -q ./../../upload/server-bundles/$VERSION/$NAME_JETTY.zip $NAME_JETTY -x "*.DS_Store"
 cd ../../
@@ -43,13 +41,10 @@ cd ../../
 # Generate checksums
 # We don't need those anymore, they are calculated and added to the download component.
 #echo 'Generating server bundles checksums in ./upload/server-bundles/'
-#shasum ./upload/server-bundles/$VERSION/$NAME_AS7.zip > ./upload/server-bundles/$VERSION/$NAME_AS7.zip.sha
-#md5 ./upload/server-bundles/$VERSION/$NAME_AS7.zip > ./upload/server-bundles/$VERSION/$NAME_AS7.zip.md5
+#shasum ./upload/server-bundles/$VERSION/$NAME_EAP.zip > ./upload/server-bundles/$VERSION/$NAME_EAP.zip.sha
+#md5 ./upload/server-bundles/$VERSION/$NAME_EAP.zip > ./upload/server-bundles/$VERSION/$NAME_EAP.zip.md5
 #shasum ./upload/server-bundles/$VERSION/$NAME_TC7.zip > ./upload/server-bundles/$VERSION/$NAME_TC7.zip.sha
 #md5 ./upload/server-bundles/$VERSION/$NAME_TC7.zip > ./upload/server-bundles/$VERSION/$NAME_TC7.zip.md5
 #shasum ./upload/server-bundles/$VERSION/$NAME_JETTY.zip > ./upload/server-bundles/$VERSION/$NAME_JETTY.zip.sha
 #md5 ./upload/server-bundles/$VERSION/$NAME_JETTY.zip > ./upload/server-bundles/$VERSION/$NAME_JETTY.zip.md5
-
-
-
 
--- packaging/release/upload.sh
+++ packaging/release/upload.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Check if version param was passed
-if [[ ! ("$#" == 1)]]; then 
+if [[ ! ("$#" == 1)]]; then
     echo 'Please pass a version number as parameter'
     exit 1
 fi
 
 export VERSION=$1
 export NAME_PREFIX=GateIn-$VERSION
-export NAME_AS7=$NAME_PREFIX-jbossas7
+export NAME_EAP=$NAME_PREFIX-jboss-eap
 export NAME_TC7=$NAME_PREFIX-tomcat7
 export NAME_JETTY=$NAME_PREFIX-jetty
 
@@ -19,7 +19,7 @@ echo
 echo
 echo 'Validate following URLs: for server bundles'
 echo
-echo "http://downloads.jboss.org/gatein/Releases/Portal/$VERSION/$NAME_AS7.zip"
+echo "http://downloads.jboss.org/gatein/Releases/Portal/$VERSION/$NAME_EAP.zip"
 echo "http://downloads.jboss.org/gatein/Releases/Portal/$VERSION/$NAME_TC7.zip"
 #echo "http://downloads.jboss.org/gatein/Releases/Portal/$VERSION/$NAME_JETTY.zip"
 
