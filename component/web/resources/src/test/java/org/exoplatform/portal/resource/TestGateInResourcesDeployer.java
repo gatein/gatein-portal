da3fb4f3d9b0636cc7148677d18675ce36d808d0
Peter Palaga <ppalaga@redhat.com>
2014-06-20 09:47:56 +0200

    GTNPORTAL-3502 Added a GateInResourcesDeployer test scenario with a repeated add and remove using interdependent apps

--- component/web/resources/src/test/java/org/exoplatform/portal/resource/TestGateInResourcesDeployer.java
+++ component/web/resources/src/test/java/org/exoplatform/portal/resource/TestGateInResourcesDeployer.java
@@ -46,6 +46,12 @@ public class TestGateInResourcesDeployer extends AbstractWebResourceTest {
     private static final String MODULE_1_SOURCE = "\ndefine('SHARED/gtnDeployerModule1', [], function() {"
             + "\nvar require = eXo.require, requirejs = eXo.require,define = eXo.define;\neXo.define.names=[];\neXo.define.deps=[];"
             + "\nreturn ccc;\n});";
+    private static final String MODULE_2_SOURCE = "\ndefine('SHARED/gtnDeployerModule2', [\"SHARED/gtnDeployerModule1\"], function(gtnDeployerModule1) {"
+            + "\nvar require = eXo.require, requirejs = eXo.require,define = eXo.define;\neXo.define.names=[\"gtnDeployerModule1\"];\neXo.define.deps=[gtnDeployerModule1];"
+            + "\nreturn ddd;\n});";
+    private static final String MODULE_2_SOURCE_WITHOUT_MODULE_1 = "\ndefine('SHARED/gtnDeployerModule2', [], function() {"
+            + "\nvar require = eXo.require, requirejs = eXo.require,define = eXo.define;\neXo.define.names=[];\neXo.define.deps=[];"
+            + "\nreturn ddd;\n});";
     private static final ResourceId M1 = new ResourceId(ResourceScope.SHARED, "gtnDeployerModule1");
     private static final ResourceId M2 = new ResourceId(ResourceScope.SHARED, "gtnDeployerModule2");
     private static final String PREFIX_1 = "/prefix1";
@@ -76,28 +82,87 @@ public class TestGateInResourcesDeployer extends AbstractWebResourceTest {
         }
     }
 
+    /**
+     * A scenario with a repeated add and remove using interdependent apps.
+     *
+     * @throws Exception
+     */
     public void testAddRemoveValid() throws Exception {
         final PortalContainer portalContainer = getContainer();
         JavascriptConfigService javascriptConfigService = portalContainer.getComponentInstanceOfType(JavascriptConfigService.class);
         SkinService skinService = portalContainer.getComponentInstanceOfType(SkinService.class);
 
+        /* ensure there is none of the two apps there initially */
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_1));
         assertNull(javascriptConfigService.getScript(M1, null));
         assertNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
-        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_1));
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        assertNull(javascriptConfigService.getScript(M2, null));
+        assertNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
 
-        WebApp webApp = new WebAppImpl(mockServletContext, Thread.currentThread().getContextClassLoader());
-        URL url = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-valid.xml");
-        deployer.add(webApp, url);
+        /* add webApp1 */
+        WebApp webApp1 = new WebAppImpl(mockServletContext, Thread.currentThread().getContextClassLoader());
+        URL url1 = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-valid.xml");
+        deployer.add(webApp1, url1);
 
         assertEquals(TARGET_PATH_1, getTargetPathMapping(javascriptConfigService, PREFIX_1));
         assertReader(TestGateInResourcesDeployer.MODULE_1_SOURCE, javascriptConfigService.getScript(M1, null));
         assertNotNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
 
-        deployer.remove(webApp);
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        assertNull(javascriptConfigService.getScript(M2, null));
+        assertNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
+
+
+        /* add webApp2 that depends on webApp1 */
+        Map<String, String> resources = new HashMap<String, String>(6);
+        resources.put("/js/gtnDeployerModule2.js", "ddd;");
+        MockJSServletContext mockServletContext2 = new MockJSServletContext("GateInResourcesDeployerApp2", resources);
+        WebApp webApp2 = new WebAppImpl(mockServletContext2, Thread.currentThread().getContextClassLoader());
+        URL url2 = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-valid-dependent.xml");
+        deployer.add(webApp2, url2);
+
+        assertEquals(TARGET_PATH_1, getTargetPathMapping(javascriptConfigService, PREFIX_1));
+        assertReader(TestGateInResourcesDeployer.MODULE_1_SOURCE, javascriptConfigService.getScript(M1, null));
+        assertNotNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
+
+        assertEquals("//mycdn.com/other-lib/6.7.8", getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        assertReader(TestGateInResourcesDeployer.MODULE_2_SOURCE, javascriptConfigService.getScript(M2, null));
+        assertNotNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
+
+        /* The crucial point: remove webApp1. It must be possible without errors, although there
+         * should be a warning in the log about removing a dependency of webApp2 */
+        deployer.remove(webApp1);
 
         assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_1));
         assertNull(javascriptConfigService.getScript(M1, null));
         assertNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
+
+        assertEquals("//mycdn.com/other-lib/6.7.8", getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        /* note that MODULE_2_SOURCE_WITHOUT_MODULE_1 does not contain the deps from webApp1 */
+        assertReader(TestGateInResourcesDeployer.MODULE_2_SOURCE_WITHOUT_MODULE_1, javascriptConfigService.getScript(M2, null));
+        assertNotNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
+
+        /* re-deployment of the webApp1 must be possible and after that, source of M2 must look like before */
+        deployer.add(webApp1, url1);
+
+        assertEquals(TARGET_PATH_1, getTargetPathMapping(javascriptConfigService, PREFIX_1));
+        assertReader(TestGateInResourcesDeployer.MODULE_1_SOURCE, javascriptConfigService.getScript(M1, null));
+        assertNotNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
+
+        assertEquals("//mycdn.com/other-lib/6.7.8", getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        assertReader(TestGateInResourcesDeployer.MODULE_2_SOURCE, javascriptConfigService.getScript(M2, null));
+        assertNotNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
+
+        /* finally remove both of them and check that there's nothing left from them */
+        deployer.remove(webApp2);
+        deployer.remove(webApp1);
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_1));
+        assertNull(javascriptConfigService.getScript(M1, null));
+        assertNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        assertNull(javascriptConfigService.getScript(M2, null));
+        assertNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
 
     }
 
@@ -110,10 +175,10 @@ public class TestGateInResourcesDeployer extends AbstractWebResourceTest {
         assertNull(javascriptConfigService.getScript(M1, null));
         assertNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
 
-        WebApp webApp = new WebAppImpl(mockServletContext, Thread.currentThread().getContextClassLoader());
-        URL url = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-with-broken-gmd-deps.xml");
+        WebApp webApp1 = new WebAppImpl(mockServletContext, Thread.currentThread().getContextClassLoader());
+        URL url1 = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-with-broken-gmd-deps.xml");
         log.info("InvalidResourceException comming...");
-        deployer.add(webApp, url);
+        deployer.add(webApp1, url1);
 
         /* none of the three was added */
         assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_1));
@@ -134,9 +199,9 @@ public class TestGateInResourcesDeployer extends AbstractWebResourceTest {
         assertNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
         assertNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
 
-        WebApp webApp = new WebAppImpl(mockServletContext, Thread.currentThread().getContextClassLoader());
-        URL url = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-valid.xml");
-        deployer.add(webApp, url);
+        WebApp webApp1 = new WebAppImpl(mockServletContext, Thread.currentThread().getContextClassLoader());
+        URL url1 = portalContainer.getPortalClassLoader().getResource("resources-deployment-service/gatein-resources-valid.xml");
+        deployer.add(webApp1, url1);
 
         assertEquals(TARGET_PATH_1, getTargetPathMapping(javascriptConfigService, PREFIX_1));
         assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_2));
@@ -160,6 +225,15 @@ public class TestGateInResourcesDeployer extends AbstractWebResourceTest {
         assertReader(TestGateInResourcesDeployer.MODULE_1_SOURCE, javascriptConfigService.getScript(M1, null));
         assertNull(javascriptConfigService.getScript(M2, null));
         assertNotNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
+        assertNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
+
+        /* cleanup */
+        deployer.remove(webApp1);
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_1));
+        assertNull(getTargetPathMapping(javascriptConfigService, PREFIX_2));
+        assertNull(javascriptConfigService.getScript(M1, null));
+        assertNull(javascriptConfigService.getScript(M2, null));
+        assertNull(skinService.getSkin("resources-deployment-service/FirstPortlet", "TestSkin"));
         assertNull(skinService.getSkin("resources-deployment-service/SecondPortlet", "TestSkin"));
 
     }
